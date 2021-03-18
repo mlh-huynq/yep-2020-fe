51ca12c27eee4dc58ec5319fae6775f9c771a214
mlh-huynq <huynq@lifetimetech.vn>
2021-03-18 16:26:55 +0700

    add bg control screen

--- src/views/Control.vue
+++ src/views/Control.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="nav">
-        <template v-for="(name, id) in prizeList">
-            <button
-                v-if="
-                    ($route.params.location === 'hn' && parseInt(id) !== 5) ||
-                        ($route.params.location === 'dn' && parseInt(id) === 5)
-                "
-                :key="id"
-                class="btn btn-primary"
-                @click="run(id)"
-                :disabled="!enabled[id]"
-            >
-                {{ name }}
-            </button>
-        </template>
+    <div class="wrapper">
+        <div class="row justify-content-center">
+            <template v-for="(name, id) in prizeList">
+                <div
+                    class="col-30 col-sm-20 my-4"
+                    v-if="
+                        ($route.params.location === 'hn' &&
+                            parseInt(id) !== 5) ||
+                            ($route.params.location === 'dn' &&
+                                parseInt(id) === 5)
+                    "
+                    :key="id"
+                >
+                    <button
+                        class="button"
+                        @click="run(id)"
+                        :disabled="!enabled[id]"
+                    >
+                        {{ name }}
+                        <template v-if="note[id]">
+                            <br />
+                            {{ note[id] }}
+                        </template>
+                    </button>
+                </div>
+            </template>
+        </div>
     </div>
 </template>
 
@@ -31,6 +43,10 @@ export default {
     data() {
         return {
             prizeList,
+            note: {
+                5: '水野さん',
+                6: '社長'
+            },
             enabled: {
                 1: false,
                 2: false,
@@ -69,16 +85,45 @@ export default {
 </script>
 
 <style lang="scss" scpoed>
-.nav {
+.wrapper {
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
     min-height: 100vh;
-    padding-bottom: 50px;
-    .btn {
-        min-width: 250px;
-        margin-bottom: 12px;
+    background-color: #ffff00;
+    background-image: url('../assets/title.png'), url('../assets/square1.png'),
+        url('../assets/square2.png');
+    background-repeat: no-repeat;
+    background-position: 0 30px, 54px 84%, 90% 0;
+    background-size: 360px auto, 50px auto, 120px auto;
+    .button-group {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        max-width: 600px;
+        .col {
+            text-align: center;
+            margin: 24px 0;
+        }
+    }
+}
+.row {
+    width: 90%;
+    max-width: 800px;
+    text-align: center;
+}
+.button {
+    background: #53575a;
+    color: #ffff00;
+    font-size: 24px;
+    width: 200px;
+    height: 100px;
+    text-transform: uppercase;
+    outline: none;
+    border: none;
+    &:disabled {
+        // background: #888b8d;
+        pointer-events: none;
     }
 }
 </style>
